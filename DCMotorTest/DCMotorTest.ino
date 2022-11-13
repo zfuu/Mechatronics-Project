f1144f1a13f59ba3fbb73d043b6db5951dc2d169
Ziyu Fu <fuziyu0730@gmail.com>
2022-11-13 23:27:09 +0900

    low power

--- DCMotorTest/DCMotorTest.ino
+++ DCMotorTest/DCMotorTest.ino
@@ -7,20 +7,15 @@ static int batteryVoltage;
 //Variable to change the motor speed and direction
 static int duty = 0;
 
-void setup()
-{
+void setup(){
   //Serial port initialization
   Serial.begin(115200);
-  //while (!Serial);
 
   //Establishing the communication with the Motor Carrier
-  if (controller.begin())
-  {
+  if (controller.begin()){
     Serial.print("Motor Carrier connected, firmware version ");
     Serial.println(controller.getFWVersion());
-  }
-  else
-  {
+  }else{
     Serial.println("Couldn't connect! Is the red LED blinking? You may need to update the firmware with FWUpdater sketch");
     while (1);
   }
@@ -37,57 +32,28 @@ void setup()
   M4.setDuty(dutyInit);
   Serial.print("Duty init: ");
   Serial.println(dutyInit);
-  //  int duty2 = dutyInit * 16777215 / 100;
-  //  Serial.print("Conversion formula: ");
-  //  Serial.println(duty2);
-  //while (1); //WHILE 1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! REMOVE!!!!
+
 }
 
 
 void loop() {
 
   //Take the battery status
-  //float batteryVoltage = (float)battery.getConverted();
+  float batteryVoltage = (float)battery.getConverted();
 
   //Reset to the default values if the battery level is lower than 11 V
-  //  if (batteryVoltage < 11)
-  //  {
-  //    Serial.println(" ");
-  //    Serial.println("WARNING: LOW BATTERY");
-  //    Serial.println("ALL SYSTEMS DOWN");
-  //    M1.setDuty(0);
-  //    M2.setDuty(0);
-  //    M3.setDuty(0);
-  //    M4.setDuty(0);
-  //    while (batteryVoltage < 11)
-  //    {
-  //      batteryVoltage = (float)battery.getConverted();
-  //    }
-  //  }
-  //  else
-  //  {
+  if (batteryVoltage < 11){
+    Serial.println(" ");
+    Serial.println("WARNING: LOW BATTERY");
+    Serial.println("ALL SYSTEMS DOWN");
+    M1.setDuty(0);
+    M2.setDuty(0);
+    while (batteryVoltage < 11){
+      batteryVoltage = (float)battery.getConverted();
+    }
+  }
 
-  //Motor test
-  //  for (duty = -100; duty < 100; duty += 1)
-  //  {
-  //    Serial.print("Motor Duty: ");
-  //    Serial.println(duty);
-  //    M1.setDuty(duty);
-  //    M2.setDuty(duty);
-  //    M3.setDuty(duty);
-  //    M4.setDuty(duty);
-  //    delay(10);
-  //  }
-  //  for (duty = 100; duty > -100; duty -= 1)
-  //  {
-  //    Serial.print("Motor Duty: ");
-  //    Serial.println(duty);
-  //    M1.setDuty(duty);
-  //    M2.setDuty(duty);
-  //    M3.setDuty(duty);
-  //    M4.setDuty(duty);
-  //    delay(10);
-  //  }
+
 
   M1.setDuty(15);
   M2.setDuty(15);
