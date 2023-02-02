b1e1a8d6ecc2ba15d56598ac3c3065b43615af66
Ziyu Fu <fuziyu0730@gmail.com>
2023-02-02 21:41:29 +0900

    before wifi integration

--- DCMotorTest/DCMotorTest.ino
+++ DCMotorTest/DCMotorTest.ino
@@ -1,13 +1,28 @@
 #include <ArduinoMotorCarrier.h>
+#include <WiFiNINA.h>
+
 #define INTERRUPT_PIN 6
+#define MAX_POWER 70.0
+
+#define DRIVE_ADJUST 0.08
+#define TURN_ADJUST 0.03
+
+#define mA -0.5
+#define mB -1.0
+#define mC 0.5
+#define mD -1.0
+
 
 //Variable to store the battery voltage
 static int batteryVoltage;
 
-//Variable to change the motor speed and direction
-static int duty = 0;
+static float drive = 0.0;
+static float turn = 0.0;
 
-int x;
+static float power_L = 0.0;
+static float power_R = 0.0;
+
+int cmd;
 
 bool w = false;
 bool a = false;
@@ -21,61 +36,183 @@ void setup(){
   Serial.setTimeout(1);
 
   // //Establishing the communication with the Motor Carrier
-  // if (controller.begin()){
-  //   Serial.print("Motor Carrier connected, firmware version ");
-  //   Serial.println(controller.getFWVersion());
-  // }else{
-  //   Serial.println("Couldn't connect! Is the red LED blinking? You may need to update the firmware with FWUpdater sketch");
-  //   while (1);
-  // }
+  if (controller.begin()){
+    Serial.print("Motor Carrier connected, firmware version ");
+    Serial.println(controller.getFWVersion());
+  }else{
+    Serial.println("Couldn't connect! Is the red LED blinking? You may need to update the firmware with FWUpdater sketch");
+    while (1);
+  }
 
-  // // Reboot the motor controller; brings every value back to default
-  // Serial.println("reboot");
-  // controller.reboot();
-  // delay(500);
+  // Reboot the motor controller; brings every value back to default
+  Serial.println("reboot");
+  controller.reboot();
+  delay(500);
 
-  // int dutyInit = 0; // at 50 it works as expected, at 60 shift sides and is too small duty to move, at 70 is very big duty.
-  // M1.setDuty(dutyInit);
-  // M2.setDuty(dutyInit);
-  // Serial.print("Duty init: ");
-  // Serial.println(dutyInit);
+  int dutyInit = 0; // at 50 it works as expected, at 60 shift sides and is too small duty to move, at 70 is very big duty.
+  M1.setDuty(dutyInit);
+  M2.setDuty(dutyInit);
+  Serial.print("Duty init: ");
+  Serial.println(dutyInit);
 
 }
 
 
 void loop() {
 
-  //Take the battery status
-  // float batteryVoltage = (float)battery.getConverted();
-
-  //Reset to the default values if the battery level is lower than 11 V
-  // if (batteryVoltage < 11){
-  //   Serial.println(" ");
-  //   Serial.println("WARNING: LOW BATTERY");
-  //   Serial.println("ALL SYSTEMS DOWN");
-  //   M1.setDuty(0);
-  //   M2.setDuty(0);
-  //   while (batteryVoltage < 11){
-  //     batteryVoltage = (float)battery.getConverted();
-  //   }
-  // }
-
-
-
-  // M1.setDuty(69);
-  // M2.setDuty(-69);
-
   while (!Serial.available());
-  x = Serial.readString().toInt();
-  // x = Serial.read();
-  Serial.print(x);
+  cmd = Serial.readString().toInt();
+  // cmd = Serial.read();
+
+  switch (cmd){
+    case 0:
+      w = false;
+      a = false;
+      s = false;
+      d = false;
+      break;
+    case 1:
+      w = true;
+      a = false;
+      s = false;
+      d = false;
+      break;
+    case 2:
+      w = false;
+      a = true;
+      s = false;
+      d = false;
+      break;
+    case 3:
+      w = false;
+      a = false;
+      s = true;
+      d = false;
+      break;
+    case 4:
+      w = false;
+      a = false;
+      s = false;
+      d = true;
+      break;
+    case 5:
+      w = true;
+      a = true;
+      s = false;
+      d = false;
+      break;
+    case 6:
+      w = true;
+      a = false;
+      s = true;
+      d = false;
+      break;
+    case 7:
+      w = true;
+      a = false;
+      s = false;
+      d = true;
+      break;
+    case 8:
+      w = false;
+      a = true;
+      s = true;
+      d = false;
+      break;
+    case 9:
+      w = false;
+      a = true;
+      s = false;
+      d = true;
+      break;
+    case 10:
+      w = false;
+      a = false;
+      s = true;
+      d = true;
+      break;
+    case 11:
+      w = true;
+      a = true;
+      s = true;
+      d = false;
+      break;
+    case 12:
+      w = true;
+      a = true;
+      s = false;
+      d = true;
+      break;
+    case 13:
+      w = true;
+      a = false;
+      s = true;
+      d = true;
+      break;
+    case 14:
+      w = false;
+      a = true;
+      s = true;
+      d = true;
+      break;
+    case 15:
+      w = true;
+      a = true;
+      s = true;
+      d = true;
+      break;
+  }
 
 
-  //Keep active the communication between Nano & Motor Carrier
-  //Ping the SAMD11
-  // controller.ping();
-  //wait
-  // delay(50);
+  if (w){drive += DRIVE_ADJUST;}
+  if (s){drive -= DRIVE_ADJUST;}
+  if (a){turn += TURN_ADJUST;}
+  if (d){turn -= TURN_ADJUST;}
+  
+  if (w && !a && !d){
+    turn = 0;
+    drive += DRIVE_ADJUST;
+  }
+  else if (s && !a && !d){
+    turn = 0;
+    drive -= DRIVE_ADJUST;
+  }
+  else if (a && !w && !s){
+    drive = 0;
+    turn += TURN_ADJUST;
+  }
+  else if (d && !w && !s){
+    drive = 0;
+    turn -= TURN_ADJUST;
+  }
+
+  if (!w && !a && !s && !d){
+    drive = 0;
+    turn = 0;
+  }
+
+
+  if (drive > 1){drive = 1;}
+  if (drive < -1){drive = -1;}
+  if (turn > 1){turn = 1;}
+  if (turn < -1){turn = -1;}
+
+  power_R = mA*drive + mB*turn;
+  power_L = mC*drive + mD*turn;
+  
+  M1.setDuty(MAX_POWER*power_R);
+  M2.setDuty(MAX_POWER*power_L);
+
+
+  Serial.print("drive:  ");
+  Serial.print(drive);
+  Serial.print("   turn:  ");
+  Serial.print(turn);
+  
+  Serial.print("  R power:  ");
+  Serial.print(MAX_POWER*power_R);
+  Serial.print("  L power:  ");
+  Serial.print(MAX_POWER*power_L);
 
 
 }
--- SerialControl.py
+++ SerialControl.py
@@ -1,14 +1,8 @@
 import serial
 import time
+import keyboard
 
 arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
-
-
-# def write_read(x):
-#     arduino.write(bytes(x, 'utf-8'))
-#     time.sleep(0.05)
-#     data = arduino.readline()
-#     return data
 
 def write(m):
     arduino.write(bytes(m, 'utf-8'))
@@ -18,10 +12,27 @@ def read():
     data = arduino.readline()
     return data
 
+
 while True:
-    message = input("Enter a number: ") # Taking input from user
-    # message = 'a'
-    write(message)
-    # value = write_read(num)
-    # print(value) # printing the value
+    wasd_state = [0, 0, 0, 0]
+
+    try:  
+        if keyboard.is_pressed('w'):
+            wasd_state[0] = 1
+        if keyboard.is_pressed('a'):
+            wasd_state[1] = 1
+        if keyboard.is_pressed('s'):
+            wasd_state[2] = 1
+        if keyboard.is_pressed('d'):
+            wasd_state[3] = 1
+    except:
+        pass 
+
+    key = tuple(wasd_state)
+
+    dict = {(0,0,0,0):0, (1,0,0,0):1, (0,1,0,0):2, (0,0,1,0):3, (0,0,0,1):4, (1,1,0,0):5, (1,0,1,0):6, (1,0,0,1):7, (0,1,1,0):8, (0,1,0,1):9, (0,0,1,1):10, (1,1,1,0):11, (1,1,0,1):12, (1,0,1,1):13, (0,1,1,1):14, (1,1,1,1):15}
+
+    # print(dict[key])
+
+    write(str(dict[key]))
     print(arduino.readline())
