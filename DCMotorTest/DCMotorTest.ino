e4ac4a1eabf1b063db403547af314b59fe070444
Ziyu Fu <fuziyu0730@gmail.com>
2023-02-07 02:38:30 +0900

    New attempt UDP arduino tutorial

--- DCMotorTest/DCMotorTest.ino
+++ DCMotorTest/DCMotorTest.ino
@@ -1,47 +1,152 @@
-#include <ArduinoMotorCarrier.h>
+#include <SPI.h>
 #include <WiFiNINA.h>
+#include <WiFiUdp.h>
 #include "arduino_secrets.h"
 
+int status = WL_IDLE_STATUS;
 
-const char* ssid = "Ben's iPhone";
-const char* password = "01234567";
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+unsigned int localPort = 2390;      // local port to listen on
+
+char packetBuffer[256]; //buffer to hold incoming packet
+char  ReplyBuffer[] = "acknowledged";       // a string to send back
 
 WiFiUDP Udp;
-unsigned int localPort = 8080;
 
 void setup() {
-  Serial.begin(115200);
+
+  //Initialize serial and wait for port to open:
+
+  Serial.begin(9600);
 
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB
+
+    ; // wait for serial port to connect. Needed for native USB port only
+
   }
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  // check for the WiFi module:
+
+  if (WiFi.status() == WL_NO_MODULE) {
+
+    Serial.println("Communication with WiFi module failed!");
+
+    // don't continue
+
+    while (true);
+
+  }
+
+  String fv = WiFi.firmwareVersion();
+
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+
+    Serial.println("Please upgrade the firmware");
+
+  }
+
+  // attempt to connect to Wifi network:
+
+  while (status != WL_CONNECTED) {
+
+    Serial.print("Attempting to connect to SSID: ");
+
+    Serial.println(ssid);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+
     delay(1000);
-    Serial.println("Connecting to WiFi...");
+
   }
 
-  Serial.println("Connected to WiFi");
+  Serial.println("Connected to wifi");
+
+  printWifiStatus();
+
+  Serial.println("\nStarting connection to server...");
+
+  // if you get a connection, report back via serial:
+
   Udp.begin(localPort);
 }
 
 void loop() {
-  char incomingPacket[255];
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
-    // read the packet into packetBufffer
-    int len = Udp.read(incomingPacket, 255);
-    if (len > 0) {
-      incomingPacket[len] = 0;
-    }
-    // print out the received message
-    Serial.println("Received packet: " + String(incomingPacket));
 
-    // Split the received message into two float numbers
-    float num1, num2;
-    sscanf(incomingPacket, "%f,%f", &num1, &num2);
-    Serial.println("First number: " + String(num1));
-    Serial.println("Second number: " + String(num2));
+  // if there's data available, read a packet
+
+  int packetSize = Udp.parsePacket();
+
+  if (packetSize) {
+
+    Serial.print("Received packet of size ");
+
+    Serial.println(packetSize);
+
+    Serial.print("From ");
+
+    IPAddress remoteIp = Udp.remoteIP();
+
+    Serial.print(remoteIp);
+
+    Serial.print(", port ");
+
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+
+    int len = Udp.read(packetBuffer, 255);
+
+    if (len > 0) {
+
+      packetBuffer[len] = 0;
+
+    }
+
+    Serial.println("Contents:");
+
+    Serial.println(packetBuffer);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+
+    Udp.write(ReplyBuffer);
+
+    Udp.endPacket();
+
   }
+}
+
+void printWifiStatus() {
+
+  // print the SSID of the network you're attached to:
+
+  Serial.print("SSID: ");
+
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+
+  IPAddress ip = WiFi.localIP();
+
+  Serial.print("IP Address: ");
+
+  Serial.println(ip);
+
+  // print the received signal strength:
+
+  long rssi = WiFi.RSSI();
+
+  Serial.print("signal strength (RSSI):");
+
+  Serial.print(rssi);
+
+  Serial.println(" dBm");
 }
